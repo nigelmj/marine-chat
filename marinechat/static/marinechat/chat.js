a26fa809eb4b621aa6b2c5912e662c5d12a774b6
Nigel Jose <nigel.jose@student.manchester.ac.uk>
2024-07-26 19:09:43 +0530

    Add type-writer effect to query replies

--- marinechat/static/marinechat/chat.js
+++ marinechat/static/marinechat/chat.js
@@ -25,41 +25,17 @@ function scrollToBottom() {
   });
 }
 
-// Function to render messages
-function appendMessage(message) {
+function appendQuery(query) {
   const md = window.markdownit();
 
   const messageDiv = document.createElement("div");
   const messageP = document.createElement("p");
 
-  messageP.innerHTML = md.render(message.fields.message);
+  messageP.innerHTML = md.render(query);
+  messageP.classList.add("query-message");
 
-  if (message.fields.sender === "user") {
-    messageDiv.classList.add("query-message-container");
-    messageP.classList.add("query-message");
-
-    messageDiv.appendChild(messageP);
-  } else {
-    const logoDiv = document.createElement("div");
-    logoDiv.classList.add("logo-circle");
-
-    const logoImg = document.createElement("img");
-    logoImg.src = "/static/marinechat/assets/ship.svg";
-    logoImg.alt = "Logo";
-    logoImg.classList.add("logo-svg");
-
-    logoDiv.appendChild(logoImg);
-
-    const replyContainer = document.createElement("div");
-    replyContainer.classList.add("query-reply-container");
-    messageP.classList.add("query-reply");
-
-    replyContainer.appendChild(messageP);
-
-    messageDiv.classList.add("query-reply-section");
-    messageDiv.appendChild(logoDiv);
-    messageDiv.appendChild(replyContainer);
-  }
+  messageDiv.appendChild(messageP);
+  messageDiv.classList.add("query-message-container");
 
   const messagesContainer = document.querySelector(".chat-container");
   messagesContainer.appendChild(messageDiv);
@@ -67,11 +43,59 @@ function appendMessage(message) {
   scrollToBottom();
 }
 
+function appendReply(message) {
+  const md = window.markdownit();
+
+  const messageDiv = document.createElement("div");
+  const messageP = document.createElement("p");
+
+  const logoDiv = document.createElement("div");
+  logoDiv.classList.add("logo-circle");
+
+  const logoImg = document.createElement("img");
+  logoImg.src = "/static/marinechat/assets/ship.svg";
+  logoImg.alt = "Logo";
+  logoImg.classList.add("logo-svg");
+
+  logoDiv.appendChild(logoImg);
+
+  const replyContainer = document.createElement("div");
+  replyContainer.classList.add("query-reply-container");
+
+  messageP.classList.add("query-reply");
+
+  replyContainer.appendChild(messageP);
+
+  messageDiv.classList.add("query-reply-section");
+  messageDiv.appendChild(logoDiv);
+  messageDiv.appendChild(replyContainer);
+
+  const messagesContainer = document.querySelector(".chat-container");
+  messagesContainer.appendChild(messageDiv);
+
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = md.render(message.fields.message);
+
+  var typing = new Typing({
+    source: tempDiv,
+    output: messageP,
+    delay: 30,
+  });
+  typing.start();
+
+  tempDiv.remove();
+}
+
 // Function to handle form submission
 function handleFormSubmission(event) {
   event.preventDefault();
   const query = document.querySelector(".query-input").value;
+
+  appendQuery(query);
+  document.querySelector(".query-input").value = "";
+
   sendQueryToServer(query);
+  removeIntro();
 }
 
 // Function to send the query to the server
@@ -97,10 +121,7 @@ function handleServerResponse(data) {
     console.error(data.error);
   } else {
     const messages = JSON.parse(data.messages);
-
-    appendMessage(messages[messages.length - 2]);
-    appendMessage(messages[messages.length - 1]);
-    document.querySelector(".query-input").value = "";
+    appendReply(messages[messages.length - 1]);
   }
 }
 
@@ -118,4 +139,11 @@ function getCookie(name) {
     }
   }
   return cookieValue;
+}
+
+function removeIntro() {
+  const intro = document.querySelector(".welcome-message");
+  if (intro) {
+    intro.remove();
+  }
 }
