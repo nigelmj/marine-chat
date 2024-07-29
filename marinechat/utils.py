7d602b047a5f47493e869dfb0f2c7367406289b1
Nigel Jose <nigel.jose@student.manchester.ac.uk>
2024-07-29 11:32:25 +0530

    Load multiple pdf documents

--- marinechat/utils.py
+++ marinechat/utils.py
@@ -9,10 +9,19 @@ from langchain_google_vertexai import ChatVertexAI
 from langchain_google_vertexai.embeddings import VertexAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# TODO: Switch to loading required PDFs
 def load_documents():
-    loader = PyPDFLoader(os.getenv("PDF_PATH", default=""))
-    return loader.load()
+    pdf_folder_path = os.getenv("PDF_FOLDER_PATH", default="")
+    if not pdf_folder_path:
+        raise ValueError("PDF_FOLDER_PATH not set in environment variables")
+
+    documents = []
+    for file in os.listdir(pdf_folder_path):
+        if file.endswith('.pdf'):
+            pdf_path = os.path.join(pdf_folder_path, file)
+            loader = PyPDFLoader(pdf_path)
+            documents.extend(loader.load())
+
+    return documents
 
 def split_documents(docs):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -29,24 +38,23 @@ def store_documents(all_splits):
     vectorstore = Chroma.from_documents(
         documents=all_splits,
         embedding=embedding,
-        persist_directory="vectordb"
+        persist_directory="pdf_embeddings"
     )
 
 def retrieve_and_generate(question):
     llm = ChatVertexAI(model="gemini-1.5-flash")
     embedding = VertexAIEmbeddings(
         model_name="textembedding-gecko@001",
-        project='ragworkflow',
+        project=os.environ.get("GOOGLE_CLOUD_PROJECT"),
     )
     vectorstore = Chroma(
-        persist_directory="vectordb",
+        persist_directory="pdf_embeddings",
         embedding_function=embedding,
     )
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
     prompt = hub.pull("rlm/rag-prompt")
 
     def format_docs(docs):
-        print(doc.page_content for doc in docs)
         return "\n\n".join(doc.page_content for doc in docs)
 
     rag_chain = (
